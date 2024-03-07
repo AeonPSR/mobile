d20af8c5f51b17fd35e5f9fc3449d0a64a399ecb
Aeon <floriandevigne1@hotmail.fr>
2024-03-07 03:50:35 -0500

    Init

---
+++ android/app/src/main/java/ionic/bases/MainActivity.java
@@ -0,0 +1,5 @@
+package ionic.bases;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.bases',
+  appName: 'bases',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
