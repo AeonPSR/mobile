d972c0a310149f847fe67f666111aa8fb5f458a1
Aeon <floriandevigne1@hotmail.fr>
2024-03-08 04:48:34 -0500

    modified:   src/pages/Register.css 	modified:   src/pages/Register.tsx

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonText, IonTextarea, IonTitle, IonToolbar, IonButton, IonInput } from '@ionic/react';
+import { IonCol, IonGrid, IonContent, IonHeader, IonPage, IonText, IonTextarea, IonTitle, IonToolbar, IonButton, IonInput, IonRow } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Register.css';
 
@@ -14,11 +14,38 @@ const Register: React.FC = () => {
 				<IonText>
 					<h1>Please create an account</h1>
 				</IonText>
-				<IonTextarea placeholder="Username" />
-				<IonTextarea placeholder="Email" />
-				<IonInput placeholder="Password" type="password" />
-				<IonInput placeholder="Re-type Password" type="password" />
-				<IonButton routerLink="/home">Send</IonButton>
+				<IonGrid>
+					<IonRow>
+						<IonCol class="icon" size="1">1</IonCol>
+						<IonCol>
+							<IonTextarea placeholder="Username" />
+						</IonCol>
+					</IonRow>
+					<IonRow>
+						<IonCol class="icon">2</IonCol>
+						<IonCol>
+							<IonTextarea placeholder="Email" />
+						</IonCol>
+					</IonRow>
+					<IonRow>
+						<IonCol class="icon">3</IonCol>
+						<IonCol>
+							<IonInput placeholder="Password" type="password" />
+						</IonCol>
+					</IonRow>
+					<IonRow>
+						<IonCol class="icon">4</IonCol>
+						<IonCol>
+							<IonInput placeholder="Re-type Password" type="password" />
+						</IonCol>
+					</IonRow>
+					<IonRow>
+						<IonCol class="icon">5</IonCol>
+						<IonCol>
+							<IonButton routerLink="/home">Send</IonButton>
+						</IonCol>
+					</IonRow>
+				</IonGrid>
 			</IonContent>
 		</IonPage>
 	);
